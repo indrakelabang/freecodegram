aa6e125d83a27f2036bb55a5a3993fc74de2a38c
gondes <gancar.ajriya@gmail.com>
2019-05-26 06:08:21 +0700

    add ProfilesController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://scontent-sin2-2.cdninstagram.com/vp/63c3bd755fa7fbbbbd51a6040a00bef9/5D655738/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com&_nc_cat=109" alt="" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>25k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
         </div>
     </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://scontent-sin2-2.cdninstagram.com/vp/8a0324942e59e32291ea913aa5f5a898/5D92951C/t51.2885-15/e35/c91.0.567.567a/59685932_782297602165270_5564603829761560709_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com&_nc_cat=11" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-sin2-2.cdninstagram.com/vp/8a0324942e59e32291ea913aa5f5a898/5D92951C/t51.2885-15/e35/c91.0.567.567a/59685932_782297602165270_5564603829761560709_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com&_nc_cat=11" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-sin2-2.cdninstagram.com/vp/8a0324942e59e32291ea913aa5f5a898/5D92951C/t51.2885-15/e35/c91.0.567.567a/59685932_782297602165270_5564603829761560709_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com&_nc_cat=11" alt="" class="w-100">
+        </div>
+    </div>
+
 </div>
 @endsection
