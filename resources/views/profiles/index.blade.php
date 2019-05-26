f37011c71c846c155543c1d095ed2ac743ff6499
gondes <gancar.ajriya@gmail.com>
2019-05-26 10:40:12 +0700

    add create post

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,15 +7,18 @@
             <img src="https://scontent-sin2-2.cdninstagram.com/vp/63c3bd755fa7fbbbbd51a6040a00bef9/5D655738/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com&_nc_cat=109" alt="" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="#">Add new Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>25k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
-            <div><a href="#">www.freecodecamp.org</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
 
