4ae8ec10e674308b725c135309f50c8af6d9944c
gondes <gancar.ajriya@gmail.com>
2019-05-27 16:17:02 +0700

    edit profile(edit profile prop)

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Intervention\Image\Facades\Image;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -22,6 +23,7 @@ class ProfilesController extends Controller
     public function update(User $user)
     {
         $this->authorize('update', $user->profile);
+
         $data = request()->validate([
             'title' => 'required',
             'description' => 'required',
@@ -29,7 +31,19 @@ class ProfilesController extends Controller
             'image' => '',
         ]);
 
-        $user->profile->update($data);
+        if(request('image'))
+        {
+            $imagePath = request('image')->store('profile', 'public');
+
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
+            $image->save();
+
+        }
+
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image' => $imagePath]
+        ));
 
         return redirect("/profile/{$user->id}");
     }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://scontent-sin2-2.cdninstagram.com/vp/63c3bd755fa7fbbbbd51a6040a00bef9/5D655738/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com&_nc_cat=109" alt="" class="rounded-circle">
+            <img src="/storage/{{ $user->profile->image }}" alt="" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
