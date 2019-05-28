c5782c2ced81d6a56d65a3ffb395d2dab196ca6f
gondes <gancar.ajriya@gmail.com>
2019-05-28 14:01:58 +0700

    add button follow in index(no action)

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -8,10 +8,16 @@
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
-                <h1>{{ $user->username }}</h1>
+
+                <div class="d-flex align-items-center pb-3">
+                    <div class="h4">{{ $user->username }}</div>
+                    <button class="btn btn-primary ml-4">Follow</button>
+                </div>
+
                 @can('update', $user->profile)
                 <a href="/p/create">Add new Post</a>
                 @endcan
+
             </div>
 
             @can('update', $user->profile)
